5de4328e18573125a88c395faf3e8a83f9df56b5
Francois Lagunas <francois@stupeflix.com>
2013-09-03 09:50:41 +0200

    Updated designs.

--- madlag3dware/madlag3dware/raspberry_case.py
+++ madlag3dware/madlag3dware/raspberry_case.py
@@ -1,7 +1,10 @@
 from solid import *
 from solid.utils import *  # Not required, but the utils module is useful
+import robothead_mount
 
-SEGMENTS = 20
+PLATE_WIDTH = 2
+
+SEGMENTS = 40
 
 RASP_TOTAL_X=85.5
 RASP_TOTAL_Y=57
@@ -78,7 +81,7 @@ def v_cale(i):
     cube([CALE_WIDTH, CALE_V_LENGTH, ENTRETOISE_HEIGHT + CALE_HEIGHT], center = False)
   )
 
-def raspberry_case():
+def raspberry_case(holes):
   holes = air_holes() + cable_output()
   entretoises = [translate(i)
                  (entretoise(BASE_PLATE_ENTRETOISE_RADIUS, BASE_PLATE_HOLE_RADIUS, h = ENTRETOISE_HEIGHT, center=True))
@@ -87,12 +90,57 @@ def raspberry_case():
   h_cales = [h_cale(i) for i in H_CALES ]
   v_cales = [v_cale(i) for i in V_CALES ]
 
-  return union()(entretoises + h_cales + v_cales) - hole()(holes)
+  return union()(entretoises + h_cales + v_cales) + hole()(holes)
+
+def y_nervure(y_size, x_shift):
+   return up(PLATE_WIDTH)(right(x_shift)(cube([PLATE_WIDTH, y_size,PLATE_WIDTH])))
+   
+def x_nervure(x_size, y_shift):
+   return left(x_size / 2.0)(up(PLATE_WIDTH)(forward(y_shift)(cube([x_size,PLATE_WIDTH,PLATE_WIDTH]))))
+
 
 if __name__ == '__main__':
-    CASE_X_SIZE = 130
-    case_width = 2
-    outer_case  = cube([CASE_X_SIZE, 100, 40]) - translate([case_width,case_width,case_width])(cube([CASE_X_SIZE - case_width * 2, 100 - case_width * 2, 40]))
+    M = 20
+    CASE_X_SIZE = 140
+    CASE_Y_SIZE = 110
+    CASE_HEIGHT = RASP_TOTAL_Z + ENTRETOISE_HEIGHT + 5
+    case_width = PLATE_WIDTH
+    outer_case  = cube([CASE_X_SIZE - M * 2, CASE_Y_SIZE - M * 2, CASE_HEIGHT])
+
+    outer_case = minkowski()(outer_case, cylinder(r=M, h=0.01))
+      
+    inner_case = translate([0,0,case_width])(cube([CASE_X_SIZE - M * 2, CASE_Y_SIZE- M * 2, CASE_HEIGHT  + 10]))
+    inner_case = minkowski()(inner_case, cylinder(r=M - case_width, h=0.01))
+
+    outer_case -= inner_case
+    outer_case = translate([+M, M, 0])(outer_case)
+    
 #    outer_case = minkowski()(outer_case, cylinder(r = 1, h = 1))
-    a = outer_case + translate([20, 20])(rotate(0)(raspberry_case()))
-    scad_render_to_file( a, "raspberry_py_case.scad", file_header='$fn = %s;' % SEGMENTS, include_orig_code=True)
+    holes = union()(robothead_mount.rasp_holes())
+
+    HOLES_Y_SHIFT = robothead_mount.RASP_HOLE_MARGIN - robothead_mount.RASP_HOLE_X_SHIFT
+    holes = translate([CASE_X_SIZE / 2, HOLES_Y_SHIFT, -50])(rotate(90, [0,0,1])(holes))
+
+    RASP_X_SHIFT = 30
+    RASP_Y_SHIFT = 20
+    a = outer_case + translate([RASP_X_SHIFT, RASP_Y_SHIFT])(rotate(0)(raspberry_case(holes)))
+    a += hole()(holes)
+
+    HEAD_HOLE_X_SIZE = 42
+    HEAD_HOLE_Y_SIZE = 30
+
+    a = left(CASE_X_SIZE / 2.0)(a)
+    
+    head_hole = cube([HEAD_HOLE_X_SIZE, HEAD_HOLE_Y_SIZE, 100], center = True)
+    a += hole()(head_hole)
+    n = y_nervure(CASE_Y_SIZE, HEAD_HOLE_X_SIZE / 2.0)
+    a += n
+    a += robothead_mount.x_symetry(n)
+
+    n = x_nervure(CASE_X_SIZE - 2, HEAD_HOLE_Y_SIZE / 2.0)
+    a += n
+    
+    n = x_nervure(CASE_X_SIZE - 2, RASP_X_SHIFT + RASP_TOTAL_Y)
+    a += n
+    
+    scad_render_to_file( a, "scad/raspberry_pi_case.scad", file_header='$fn = %s;' % SEGMENTS, include_orig_code=True)
--- madlag3dware/madlag3dware/robothead_mount.py
+++ madlag3dware/madlag3dware/robothead_mount.py
@@ -2,8 +2,6 @@ import raspberry_case
 from solid import *
 from solid.utils import *  # Not required, but the utils module is useful
 
-import robothead_mount
-
 SEGMENTS = 100
 
 BASE_Y_LEN=58
@@ -25,10 +23,13 @@ HOLE_FRONT_Y_SHIFT=10
 HOLE_BACK_X_SHIFT=32.5
 HOLE_BACK_Y_SHIFT=10
 HOLE_RADIUS=1.75
-RASP_HOLE_X_SHIFT=40
-RASP_HOLE_Y_SHIFT=15
+RASP_HOLE_MARGIN=5
+RASP_HOLE_X_SHIFT=RASP_PLATE_X_SHIFT + BACK_X_SHIFT + RASP_HOLE_MARGIN
+RASP_HOLE_X_SHIFT_BACK=RASP_HOLE_X_SHIFT + 15
+RASP_HOLE_Y_SHIFT=25
 RASP_HOLE_RADIUS=1.75
 
+RASP_PLATE_CUT_Y_LEN=40
 
 def parallelogram(w,h, skew, plate_width):
   return linear_extrude(height = plate_width)(
@@ -39,7 +40,30 @@ def parallelogram(w,h, skew, plate_width):
 def y_symetry(obj):
   return scale([1,-1,1])(obj)
 
+def x_symetry(obj):
+  return scale([-1,1,1])(obj)
+
 RASP_FIX_PLATE_WIDTH=3
+
+def rasp_holes():
+  return [
+    translate([RASP_HOLE_X_SHIFT, -RASP_HOLE_Y_SHIFT, 55]) (
+        cylinder(r = RASP_HOLE_RADIUS, h = 80, center = True)
+    ),
+
+    translate([RASP_HOLE_X_SHIFT, RASP_HOLE_Y_SHIFT, 55]) (
+        cylinder(r = RASP_HOLE_RADIUS, h = 80, center = True)
+    ),
+
+
+    translate([RASP_HOLE_X_SHIFT_BACK, -RASP_HOLE_Y_SHIFT, 55]) (
+        cylinder(r = RASP_HOLE_RADIUS, h = 80, center = True)
+    ),
+
+    translate([RASP_HOLE_X_SHIFT_BACK, RASP_HOLE_Y_SHIFT, 55]) (
+        cylinder(r = RASP_HOLE_RADIUS, h = 80, center = True)
+    )]
+
 
 def head_mount():
   base = left(FRONT_X_SHIFT)(forward(-BASE_Y_LEN / 2.0)
@@ -53,6 +77,7 @@ def head_mount():
   
   rear_reinforce_l = back(BASE_Y_LEN / 2.0) (p)
 
+  # reinforce elements on base plate
   p2 = parallelogram(SUPPORT_X_LEN,
                      RASP_PLATE_HEIGHT / 4.0,
                      RASP_PLATE_X_SHIFT / 4.0,
@@ -61,30 +86,31 @@ def head_mount():
   rear_reinforce_base = p2
   rear_reinforce_base = forward(BASE_Y_LEN / 2.0)(rear_reinforce_base)
 
+  # cut in p2
   p3 = parallelogram(SUPPORT_X_LEN,
                      RASP_PLATE_HEIGHT / 3.9,
                      RASP_PLATE_X_SHIFT / 3.9,
                      BASE_Y_LEN - REINFORCE_WIDTH * 2.0)
   p3 = rotate([90,0,0])(p3)
+  # do the cut p2 - p3
   rear_reinforce_base -= forward(BASE_Y_LEN / 2.0 - REINFORCE_WIDTH)(left(REINFORCE_WIDTH)(p3))
-    
+
+  # Total reinforce elements on base plate
   rear_reinforce = rear_reinforce_l + y_symetry(rear_reinforce_l)
 
   rear_reinforce = right(BACK_X_SHIFT)(rear_reinforce + rear_reinforce_base)
 
+  # raspberry pi fixation plate
   rasp_plate = right(BACK_X_SHIFT + RASP_PLATE_X_SHIFT)(forward(-BASE_Y_LEN / 2.0 - PLATE_WIDTH)
                                    (cube([SUPPORT_X_LEN, BASE_Y_LEN + 2 * PLATE_WIDTH, PLATE_WIDTH])))
 
   rasp_plate = up(RASP_PLATE_HEIGHT)(rasp_plate)
 
+  rasp_plate_cut = translate([BACK_X_SHIFT, -RASP_PLATE_CUT_Y_LEN / 2,0]) (cube([SUPPORT_X_LEN * 2 + RASP_PLATE_X_SHIFT, RASP_PLATE_CUT_Y_LEN, 40]))
   
+  rasp_plate -= rasp_plate_cut
 
-#  rasp_plate = up(PLATE_WIDTH)
-  
-
-  servo_hole = translate([0,0, 5]) (
-    cylinder(r = (SERVO_HEAD_DIAMETER + SERVO_HEAD_DIAMETER_MARGIN)/ 2.0, h = 100, center = True)
-    )
+  servo_hole = up(5)(cylinder(r = (SERVO_HEAD_DIAMETER + SERVO_HEAD_DIAMETER_MARGIN)/ 2.0, h = 100, center = True))
 
   front_hole_a = translate([-HOLE_FRONT_X_SHIFT,0, 5]) (
         cylinder(r = HOLE_RADIUS, h = 100, center = True)
@@ -100,30 +126,16 @@ def head_mount():
         cylinder(r = HOLE_RADIUS, h = 20, center = True)
     )
 
-  rasp_hole_a = translate([RASP_HOLE_X_SHIFT, -RASP_HOLE_Y_SHIFT, 55]) (
-        cylinder(r = RASP_HOLE_RADIUS, h = 80, center = True)
-    )
-
-  rasp_hole_b = translate([RASP_HOLE_X_SHIFT, RASP_HOLE_Y_SHIFT, 55]) (
-        cylinder(r = RASP_HOLE_RADIUS, h = 80, center = True)
-    )
-
-
-  rasp_hole_c = translate([RASP_HOLE_X_SHIFT + 15, -RASP_HOLE_Y_SHIFT, 55]) (
-        cylinder(r = RASP_HOLE_RADIUS, h = 80, center = True)
-    )
-
-  rasp_hole_d = translate([RASP_HOLE_X_SHIFT + 15, RASP_HOLE_Y_SHIFT, 55]) (
-        cylinder(r = RASP_HOLE_RADIUS, h = 80, center = True)
-    )
-
-
   head_mount = base + rear_reinforce + rasp_plate
+
+  holes = front_hole_a + front_hole_b + servo_hole + back_hole_a + back_hole_b
+  for r in rasp_holes():
+    holes += r
     
-  return head_mount - hole()(front_hole_a + front_hole_b + servo_hole + back_hole_a + back_hole_b + rasp_hole_a + rasp_hole_b + rasp_hole_c + rasp_hole_d)
+  return head_mount - hole()(holes)
 
 
 if __name__ == '__main__':
     a = head_mount()
-    scad_render_to_file( a, "robot_head_mount_2.scad", file_header='$fn = %s;' % SEGMENTS)
+    scad_render_to_file( a, "scad/robothead_mount.scad", file_header='$fn = %s;' % SEGMENTS)
   
